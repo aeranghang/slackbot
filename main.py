a887c18add4065ba0787790af8c8bc3cf1878a17
aeranghang <aeranghang@gmail.com>
2018-08-07 16:08:43 +0900

    슬랙 연동

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Be strong...")
-print("웅앵웅?")
+import random
+
+from rtmbot import RtmBot
+from rtmbot.core import Plugin
+
+import secret
+
+
+class HelloPlugin(Plugin):
+    def process_message(self, data):
+        if "애란" in data["text"]:
+            self.outputs.append([data["channel"], "불렀어?"])
+        elif "주사위" == data["text"]:
+            self.outputs.append([data["channel"], "주사위를 던졌더니 " + str(random.randint(1, 6)) + " 나왔다."])
+
+
+config = {
+    "SLACK_TOKEN": secret.SLACK_TOKEN,
+    "ACTIVE_PLUGINS": ["main.HelloPlugin"]
+}
+bot = RtmBot(config)
+bot.start()
