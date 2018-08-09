90b328db9b41c640ab82fb6470a3633ce004b282
aeranghang <aeranghang@gmail.com>
2018-08-09 11:27:18 +0900

    슬랙 API와 대화 로직을 분리

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ from rtmbot.core import Plugin
 import secret
 
 
+def answer(text):
+    if "애란" in text:
+        reply = "불렀어??"
+    elif "주사위" == text:
+        reply = str(random.randint(1, 6))
+    else:
+        reply = None
+    return reply
+
+
 class HelloPlugin(Plugin):
     def process_message(self, data):
-        if "애란" in data["text"]:
-            self.outputs.append([data["channel"], "불렀어?"])
-        elif "주사위" == data["text"]:
-            die = str(random.randint(1, 6))
-            self.outputs.append([data["channel"], die])
-        else:
-            pass
+        reply = answer(data["text"])
+        if reply is not None:
+            self.outputs.append([data["channel"], reply])
 
 
 config = {
