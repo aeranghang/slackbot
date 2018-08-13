4b3daaac67e40242cb63424b0a2b5b6d1591ec17
aeranghang <aeranghang@gmail.com>
2018-08-14 05:20:47 +0900

    말머리를 설정으로 분리

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ def answer(text):
 class HelloPlugin(Plugin):
     def process_message(self, data):
         # 메시지를 말머리(prefix)와 나머지 부분(rest)으로 분리
-        prefix, rest = data["text"][:3], data["text"][3:]
+        len_prefix = len(config["PREFIX"])
+        prefix, rest = data["text"][:len_prefix], data["text"][len_prefix:]
 
         # 말머리가 일치하지 않으면 응답하지 않음
-        if prefix != "애란 ":
+        if prefix != config["PREFIX"]:
             return
 
         # 응답
@@ -31,6 +32,7 @@ class HelloPlugin(Plugin):
 
 config = {
     "SLACK_TOKEN": secret.SLACK_TOKEN,
+    "PREFIX": "애란 ",
     "ACTIVE_PLUGINS": ["main.HelloPlugin"]
 }
 bot = RtmBot(config)
